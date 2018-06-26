2e4658a0607ffe08a840b3106838c0f801e6e0d0
SvetlanaRonzina <swet.ronzina@yandex.ru>
2018-06-26 17:28:11 +0500

    Задача 2

---
+++ Задача 2/Задача 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace Задача_2
+{
+    class Program
+    {
+
+        static bool Winner(int day, int month)
+        {
+            if (((day % 3 == 0) && (month % 2 == 0)) ||
+            ((day % 3 != 0) && (month % 2 != 0)))
+                return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("INPUT.TXT").Split(' ');
+            var day = int.Parse(data[0]);
+            var month = int.Parse(data[1]);
+
+            File.WriteAllText("OUTPUT.TXT", Winner(day, month) ? $"{2}" : $"{1}");
+        }
+    }
+}
